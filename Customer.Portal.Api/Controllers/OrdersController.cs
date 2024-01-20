83f7239333d683e39c7ff0f52705d3f837c96585
pradeep dm <pradeep.dm@frontiersin.net>
2024-01-20 19:50:38 +0530

    Refactoring : Removed unused pvt mapping method after automapper implementation

--- Customer.Portal.Api/Controllers/OrdersController.cs
+++ Customer.Portal.Api/Controllers/OrdersController.cs
@@ -54,48 +54,7 @@ namespace Customer.Portal.Api.Controllers
         #endregion
 
         #region PRIVATE METHODS
-        private static HttpStatusCode GetPreconditionFailed()
-        {
-            return HttpStatusCode.PreconditionFailed;
-        }
-
-        private IEnumerable<ViewModels.OrderItems> MapOrderItems(IEnumerable<DataAccess.Read.Models.OrderItems> lastOrderProductDetails)
-        {
-            var allOrderItems = new List<ViewModels.OrderItems>();
-            foreach (var p in lastOrderProductDetails)
-            {
-                var orderItems = new ViewModels.OrderItems();
-                orderItems.Product = p.Product;
-                orderItems.Quantity = p.Quantity;
-                orderItems.PriceEach = p.PriceEach;
-                allOrderItems.Add(orderItems);
-            }
-            return allOrderItems;
-        }
-
-        private CustomerRecentOrderOutputViewModel MapTOCustomerRecentOrderViewModel(CustomerOrderDetails recentOrderDetails)
-        {
-            CustomerRecentOrderOutputViewModel result = new CustomerRecentOrderOutputViewModel();
-
-            ViewModels.CustomerDetails customerData = new ViewModels.CustomerDetails()
-            {
-                FirstName = recentOrderDetails.Customer.FirstName,
-                LastName = recentOrderDetails.Customer.LastName
-            };
-            ViewModels.OrderDetails? orderDetails = recentOrderDetails.Order is null ? null : new ViewModels.OrderDetails()
-            {
-                OrderNumber = recentOrderDetails.Order.OrderNumber,
-                OrderDate = recentOrderDetails.Order.OrderDate,
-                DeliveryAddress = recentOrderDetails.Order.DeliveryAddress,
-                OrderItems = _mapper.Map<IEnumerable<ViewModels.OrderItems>> (recentOrderDetails.Order.OrderItems),
-                DeliveryExpected = recentOrderDetails.Order.DeliveryExpected
-            };
-
-
-            result.Customer = customerData;
-            result.Order = orderDetails;
-            return result;
-        }
+    
         #endregion
     }
 }
